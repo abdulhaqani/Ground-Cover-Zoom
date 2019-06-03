1b2b7153374e56a958d8d8efd3be2f5c66430b17
Abdul Haqani <ahaqa011@uottawa.ca>
2019-06-03 12:21:24 -0400

    took out auth for now

--- routes/displayForms.js
+++ routes/displayForms.js
@@ -2,41 +2,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const glob = require('glob');
+const path = require('path');
+const serveIndex = require('serve-index');
 
 const router = express.Router();
 const { ensureAuthenticated } = require('../helpers/auth');
-
-const images = glob(
-  './public/pictures/{*.jpg,*.png,*.gif, *.jpeg}',
-  glob.GLOB_BRACE
-);
-
 require('../models/DisplayForms');
+
+router.use('/images', serveIndex(path.join(__dirname, '/images')));
 
 const DisplayForms = mongoose.model('displayForms');
 
 // DisplayForms index page
-router.get('/', ensureAuthenticated, (req, res) => {});
+router.get('/', (req, res) => {
+  res.render('displayForms/index');
+});
 
 // Add displayForms form
-router.get('/add', ensureAuthenticated, (req, res) => {
+router.get('/add', (req, res) => {
   res.render('displayForms/add');
 });
 
 // edit displayForms form
-router.get('/edit/:id', ensureAuthenticated, (req, res) => {});
+router.get('/edit/:id', (req, res) => {});
 
 // process form
-router.post('/', ensureAuthenticated, (req, res) => {
-  const newUser = {};
-  new DisplayForms(newUser).save().then(() => {
+router.post('/', (req, res) => {
+  const newImage = {};
+  new DisplayForms(newImage).save().then(() => {
     req.flash('success_msg', 'Image added');
     res.redirect('/displayForms');
   });
 });
 
 // edit form process
-router.put('/edit/:id', ensureAuthenticated, (req, res) => {});
+router.put('/edit/:id', (req, res) => {});
 
 /* // Delete displayForms
 router.delete('/:id', ensureAuthenticated, (req, res) => {
