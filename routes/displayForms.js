9f8abdea66f26b4a45d68f4ac853d539c24da323
Abdul Haqani <ahaqa011@uottawa.ca>
2019-06-03 12:03:38 -0400

    added display schema and removed unneeded parts

--- routes/displayForms.js
+++ routes/displayForms.js
@@ -1,24 +1,22 @@
 /* eslint-disable eqeqeq */
 const express = require('express');
 const mongoose = require('mongoose');
+const glob = require('glob');
 
 const router = express.Router();
 const { ensureAuthenticated } = require('../helpers/auth');
+
+const images = glob(
+  './public/pictures/{*.jpg,*.png,*.gif, *.jpeg}',
+  glob.GLOB_BRACE
+);
 
 require('../models/DisplayForms');
 
 const DisplayForms = mongoose.model('displayForms');
 
 // DisplayForms index page
-router.get('/', ensureAuthenticated, (req, res) => {
-  DisplayForms.find({ user: req.user.id })
-    .sort({ date: 'desc' })
-    .then(displayForms => {
-      res.render('displayForms/index', {
-        displayForms,
-      });
-    });
-});
+router.get('/', ensureAuthenticated, (req, res) => {});
 
 // Add displayForms form
 router.get('/add', ensureAuthenticated, (req, res) => {
@@ -26,71 +24,26 @@ router.get('/add', ensureAuthenticated, (req, res) => {
 });
 
 // edit displayForms form
-router.get('/edit/:id', ensureAuthenticated, (req, res) => {
-  DisplayForms.findOne({
-    _id: req.params.id,
-  }).then(displayForm => {
-    if (displayForm.user != req.user.id) {
-      req.flash('error_msg', 'Not authorized');
-      res.redirect('/displayForms');
-    } else {
-      res.render('displayForms/edit', {
-        displayForm,
-      });
-    }
-  });
-});
+router.get('/edit/:id', ensureAuthenticated, (req, res) => {});
 
 // process form
 router.post('/', ensureAuthenticated, (req, res) => {
-  const errors = [];
-  if (!req.body.title) {
-    errors.push({ text: 'please add a title' });
-  }
-  if (!req.body.details) {
-    errors.push({ text: 'please add a details' });
-  }
-
-  if (errors.length > 0) {
-    res.render('displayForms/add', {
-      errors,
-      title: req.body.title,
-      details: req.body.details,
-    });
-  } else {
-    const newUser = {
-      title: req.body.title,
-      details: req.body.details,
-      user: req.user.id,
-    };
-    new DisplayForms(newUser).save().then(() => {
-      req.flash('success_msg', 'displayForms added');
-      res.redirect('/displayForms');
-    });
-  }
-});
-
-// edit form process
-router.put('/edit/:id', ensureAuthenticated, (req, res) => {
-  DisplayForms.findOne({
-    _id: req.params.id,
-  }).then(displayForms => {
-    displayForms.title = req.body.title;
-    displayForms.details = req.body.details;
-
-    displayForms.save().then(() => {
-      req.flash('success_msg', 'displayForms updated');
-      res.redirect('/displayForms');
-    });
+  const newUser = {};
+  new DisplayForms(newUser).save().then(() => {
+    req.flash('success_msg', 'Image added');
+    res.redirect('/displayForms');
   });
 });
 
-// Delete displayForms
+// edit form process
+router.put('/edit/:id', ensureAuthenticated, (req, res) => {});
+
+/* // Delete displayForms
 router.delete('/:id', ensureAuthenticated, (req, res) => {
   DisplayForms.deleteOne({ _id: req.params.id }).then(() => {
     req.flash('success_msg', 'displayForms removed');
     res.redirect('/displayForms');
   });
-});
+}); */
 
 module.exports = router;
