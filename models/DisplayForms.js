c4cf00d71b612a98dc28d46333eab53d49625c48
Abdul Haqani <ahaqa011@uottawa.ca>
2019-06-26 15:57:15 -0400

    edited mongo schema

--- models/DisplayForms.js
+++ models/DisplayForms.js
@@ -3,12 +3,32 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const DisplaySchema = new Schema({
-  FileName: String,
-  Classified: Boolean,
-  SolarPanel: Boolean,
-  GreenHouse: Boolean,
-  Lat: String,
-  Lon: String,
+  User: {
+    type: String,
+    required: true,
+  },
+  FileName: {
+    type: String,
+    required: true,
+  },
+  Classified: {
+    type: String,
+    required: true,
+  },
+  SolarPanel: {
+    type: String,
+    required: true,
+  },
+  GreenHouse: {
+    type: String,
+    required: true,
+  },
+  Lat: {
+    type: String,
+  },
+  Lon: {
+    type: String,
+  },
 });
 
 mongoose.model('displayForms', DisplaySchema);
