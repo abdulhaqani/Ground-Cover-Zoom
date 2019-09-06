76ffb1cb77fba611cacd01f809aea326387a02b1
Abdul Haqani <ahaqa011@uottawa.ca>
2019-09-05 21:28:22 -0400

    Online db configured

--- keys.js
+++ keys.js
@@ -1,2 +1,8 @@
 const MongoURI =
   'mongodb+srv://AbdulHaqani:Dudeperfect1@greenhouse-dev-l5lba.mongodb.net/test?retryWrites=true&w=majority';
+const MongoLocal = 'mongodb://localhost/vidjot-dev'
+
+module.exports = {
+  MongoURI,
+  MongoLocal,
+};
