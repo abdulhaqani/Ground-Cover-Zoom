9116b05b88882dfa29a937c578855c0f9cb65ddf
abdulhaqani <ahaqa011@uottawa.ca>
2019-09-13 13:54:13 -0400

    push

--- keys.js
+++
@@ -1,8 +0,0 @@
-const MongoURI =
-  'mongodb+srv://Abdul:StatsCan1@greenhouse-classification-uqmrl.mongodb.net/test?retryWrites=true&w=majority';
-const MongoLocal = 'mongodb://localhost/vidjot-dev';
-
-module.exports = {
-  MongoURI,
-  MongoLocal,
-};
