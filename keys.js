6febc4eb8ce1594f4ed56275799751c5d841a52b
abdulhaqani <sara.burns@cloud.statcan.ca>
2019-09-13 13:51:04 -0400

    push

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 const MongoURI =
-  'mongodb+srv://AbdulHaqani:Dudeperfect1@greenhouse-dev-l5lba.mongodb.net/test?retryWrites=true&w=majority';
+  'mongodb+srv://Abdul:StatsCan1@greenhouse-classification-uqmrl.mongodb.net/test?retryWrites=true&w=majority';
 const MongoLocal = 'mongodb://localhost/vidjot-dev';
 
 module.exports = {
